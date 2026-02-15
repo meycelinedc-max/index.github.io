6b305c0d28a49bc21b240e7cb9130faf7dd6b98d
meycelinedc-max <meycelinedc@gmail.com>
2026-02-16 07:35:53 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function upDate(previewPic){
+    console.log("Mouse over triggered");
+    console.log("Alt text: " + previewPic.alt);
+    console.log("Source: " + previewPic.src);
+
+    document.getElementById("image").innerHTML = previewPic.alt;
+
+    document.getElementById("image").style.backgroundImage =
+        "url('" + previewPic.src + "')";
+}
+
+function undo(){
+    document.getElementById("image").innerHTML =
+        "Hover over an image below to display here.";
+
+    document.getElementById("image").style.backgroundImage = "url('')";
+}
